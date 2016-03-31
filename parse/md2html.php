037a1b28841a5b903dce637ae4e289d53e8f4bdb
Yaoguais <newtopstdio@163.com>
2016-03-31 16:24:27 +0000

    md2html files, change homepage

--- parse/md2html.php
+++ parse/md2html.php
@@ -5,13 +5,23 @@ $tpl = file_get_contents(__DIR__ . '/article.tpl');
 $root = realpath(__DIR__ . '/../');
 
 $parseDown = new Parsedown();
+
+$navFile = $root . '/md/index.md';
+$toFile = str_replace('/md/index.md', '/index.html', $navFile);
+$html = $parseDown->text(file_get_contents($navFile));
+$html = preg_replace('/\?s=md\/(.+?)\/(.+?)\.md/', '/article/\1/\2.html', $html);
+preg_match_all('/\/article\/(.+?)\/(.+?)\.html/', $html, $matches);
+$navFiles = array_map(function ($val) use ($root) {
+    return $root . $val;
+}, $matches[0]);
+
 $mdFiles = getFiles($root . '/md');
-
 foreach ($mdFiles as $file) {
-    if(strrpos($file, '/md/index.md')){
-
-    }else{
+    if (!strrpos($file, '/md/index.md')) {
         $toFile = str_replace(['/md/', '.md'], ['/article/', '.html'], $file);
+        if (!in_array($toFile, $navFiles)) {
+            echo '[error] ',$file,"\n";
+        }
         $toFileDir = dirname($toFile);
         if (!file_exists($toFileDir)) {
             mkdir($toFileDir);
@@ -21,6 +31,7 @@ foreach ($mdFiles as $file) {
         file_put_contents($toFile, $finalHtml);
     }
 }
+echo count($navFiles), '/', count($mdFiles) - 1, "\n";
 echo "done\n";
 
 function getFiles($dir)
